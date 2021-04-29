7a7d38bf8d9d4fd48fc580588d42b8a26c2f17bc
juliajungle <julesrlax@gmail.com>
2021-04-29 16:16:37 +0100

    feature

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,10 +1,27 @@
 import { Scenario } from "./Scenario";
+import styled from "styled-components";
+
+const StyledFeatureTitle = styled.div`
+  font-size: 32px;
+  padding: 0;
+  margin: 0;
+  background-color: #51ddb4;
+
+  h3 {
+    position: relative;
+    top: -23px;
+    right: 11px;
+  }
+`;
 
 export const Feature = ({ name, userStory, scenarios }) => {
   console.log({ scenarios });
   return (
     <div>
-      <h3>{name}</h3>
+      <StyledFeatureTitle>
+        <h3>{name}</h3>
+      </StyledFeatureTitle>
+
       <p>{userStory}</p>
       {scenarios.map(({ scenarioName, scenarioSteps }) => {
         return (
