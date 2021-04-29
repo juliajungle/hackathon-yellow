c2d866e2cbf48caf82fd5e2971bbc0b25c8e1a68
juliajungle <julesrlax@gmail.com>
2021-04-29 14:35:19 +0100

    added styled-components

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header"></header>
       <div>
-        <Nav></Nav>
+        <Nav />
         <p>Hello world!</p>
       </div>
     </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,22 @@
+import styled from "styled-components";
+
+const StyledNav = styled.nav`
+  font-size: 44px;
+
+  ul {
+  }
+
+  li {
+  }
+`;
+
 export const Nav = () => {
   return (
-    <ul>
-      <li>Feature 1</li>
-      <li>Feature 2</li>
-    </ul>
+    <StyledNav>
+      <ul>
+        <li>Feature 1</li>
+        <li>Feature 2</li>
+      </ul>
+    </StyledNav>
   );
 };
